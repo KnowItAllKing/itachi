e5d26f79f189ba00b669753493f77ea760390bfd
KnowItAllKing <rieffelkai@gmail.com>
2019-02-10 22:14:27 -0500

    First commit

--- dist/structures/base/Event.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Event {
-    // public execute!: (client: Itachi, ...params: any) => Promise <void>;
-    constructor(options) { Object.assign(this, options); }
-    ;
-}
-exports.Event = Event;
-;
